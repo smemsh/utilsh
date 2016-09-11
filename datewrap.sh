077ef1f44fee52c352a561a28faea64f8693f3b5
Scott Mcdermott <scott@smemsh.net>
2016-09-11 08:17:51 -0700

    + new: datewrap.sh: convenience wraps of /bin/date
    split off from pomotimer, these are generic enough and used enough that
    they should be in the utilsh repo

---
+++ datewrap.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# datewrap.sh
+#   now, today, yesterday, tomorrow, thisweek, lastweek, thismonth, lastmonth
+#
+# desc:
+#   convenience date wrappers
+#
+# scott@smemsh.net
+# http://smemsh.net/src/utilsh/
+# http://spdx.org/licenses/GPL-2.0
+#
+##############################################################################
+
+invname=${0##*/}
+
+mflags=%Y%m
+dflags=%Y%m%d
+wflags=%G%V
+nflags=%Y%m%d%H%M%S
+
+_thisweek  () { date +$wflags; }
+_thismonth () { date +$mflags; }
+_today     () { date +$dflags; }
+_now       () { date +$nflags; }
+
+_lastweek  () { date -d last-week +$wflags; }
+_lastmonth () { date -d last-month +$mflags; }
+_yesterday () { date -d yesterday +$dflags; }
+_tomorrow  () { date -d tomorrow +$dflags; }
+
+main ()
+{
+	if [[ $(declare -F _$invname) ]]; then
+		_$invname "$@"
+	else
+		echo usage:
+		for f in $(compgen -A function _); do
+			echo -e \\t${f#_}; done
+		exit 1
+	fi
+}
+
+main "$@"
