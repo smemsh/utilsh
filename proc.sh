936d9c362ce852c02f9517a3928e93adf46c6484
Scott Mcdermott <scott@smemsh.net>
2016-09-11 08:06:33 -0700

    + proc.sh: new util to get process lists / table
    psa wrapper from our shell, more usable standalone (and usable when
    invoke through sudo, unlike a shell function)

---
+++ proc.sh
@@ -0,0 +1,55 @@
+#
+# proc.sh: psa, psf, procs
+#   select and display particular processes
+#
+# scott@smemsh.net
+# http://smemsh.net/src/utilsh/
+# http://spdx.org/licenses/GPL-2.0
+#
+##############################################################################
+
+# by name, or give full process table without arg
+#
+psa ()
+{
+	local psargs grepargs
+	local pids
+
+	(($#)) && grepargs="[${1:0:1}]${1:1}" # TODO works? such old code!
+
+	local uname=`uname -s`
+	# TODO: use *TYPE variables provided by bash
+	if [[ $uname == 'Linux' ]]; then
+		if pids=`pgrep -f "$*" -d,`; then
+			ps -Ho pid,pcpu,rss,vsz,tty,s,cmd -p $pids; fi
+		return
+	elif [[ $name == 'SunOS' ]]; then psargs=ef
+	elif [[ $name == 'Darwin' ]]; then psargs=efwwwwww
+	else echo "no code for you!"
+	fi
+
+	ps -$psargs | egrep "$grepargs"
+}
+
+# processes with a session leader matching specified name
+#
+psf ()
+{
+	ps -H --sid $(pgrep -d, $@) -F
+}
+
+# tabular list of unique process names except kernel threads
+#
+procs ()
+{
+	ps -N --ppid=2 -o comm= |
+	sort -u | column | column -t
+}
+
+##############################################################################
+
+invoke=${0##*/}
+if ! [[ `declare -F $invoke` ]]; then
+	echo "unimplemented"; false; exit; fi
+
+$invoke "$@"
